76aac05926892e5c873d6da3fcaf78f0375a2782
Kenneth Cheung <kenche@perfec.com>
2024-01-28 12:46:30 -0500

    search11

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       #return render_template('hello.html', name = name)
+       return render_template('hello.html', name = name)
        
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
