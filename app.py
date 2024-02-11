d31ca960161302e65f6e0fbf944e326cd06d62dd
Kenneth Cheung <kenche@perfec.com>
2024-02-10 19:26:51 -0500

    app.py 1000char

--- app.py
+++ app.py
@@ -69,28 +69,11 @@ def hello():
    i = 0        
    for result in results:
         tempContent = result["content"]    
-#        tempContent = tempContent[0:1000]  
+        tempContent = tempContent[0:1000]  
+        tempOutput = tempOutput + result["metadata_spo_item_name"] + ";;" + str(round(result["@search.reranker_score"],2)) + ";;" + tempContent + ",,"
         
 #        bert_model = Summarizer()
 #        ext_summary = bert_model(tempContent, ratio=0.5)
-
-
-        tokens_input = tokenizer.encode("summarize: " + tempContent,
-                                        return_tensors='pt',
-                                        max_length=tokenizer.model_max_length,
-                                        truncation=True)
-
-
-        summary_ids = model.generate(tokens_input, min_length=80, 
-                                    max_length=150, length_penalty=15, 
-                                    num_beams=2)
-        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-
-        print(summary)
-        tempContent = summary
-
-
-        tempOutput = tempOutput + result["metadata_spo_item_name"] + ";;" + str(round(result["@search.reranker_score"],2)) + ";;" + tempContent + ",,"
 
 
    myRows = tempOutput.split(",,")      
